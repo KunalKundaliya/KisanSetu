6650134d3f0fcf0fffc570b2498ffbc4c5562891
infinity-0008 <abhay.20253009@mnnit.ac.in>
2026-09-02 15:40:17 +0530

    backend-structure-fix-3

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -18,24 +17,4 @@ const startServer = async () => {
 };
 
 startServer();
-=======
-import dotenv from "dotenv";
-dotenv.config();
 
-import app from "./app.js";
-import connectDB from "./db/index.db.js";
-import { PORT } from "./constants.js";
-import logger from "./utils/logger.js";
-
-const startServer = async () => {
-  await connectDB();
-
-  const port = process.env.PORT || PORT || 5000;
-  app.listen(port, "0.0.0.0", () => {
-    logger.info(`Kisan Setu API running on port ${port}`);
-    logger.info(`Health check: http://localhost:${port}/api/v1/health`);
-  });
-};
-
-startServer();
->>>>>>> 66a59f3146aff9b1e82c27738dfc667307e29d51
